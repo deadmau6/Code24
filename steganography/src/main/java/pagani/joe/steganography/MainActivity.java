f59b556503c345ba23899f15ddbec4cb47ff37f9
deadmau6 <joepop16@gmail.com>
2016-10-08 16:47:12 -0600

    Main menu view

--- steganography/src/main/java/pagani/joe/steganography/MainActivity.java
+++ steganography/src/main/java/pagani/joe/steganography/MainActivity.java
@@ -1,13 +1,37 @@
 package pagani.joe.steganography;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.content.Context;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener{
+    private static Context context;
+    private Button encrypt;
+    private Button decrypt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+        encrypt = (Button) findViewById(R.id.encryptBtn);
+        decrypt = (Button) findViewById(R.id.decryptBtn);
+
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.encryptBtn:
+                //go encrypt
+                break;
+            case R.id.decryptBtn:
+                //go decrypt
+                break;
+        }
     }
 }

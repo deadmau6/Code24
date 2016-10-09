068f119f4c9b07c9a4c5fb84875fad4af40adffc
TheTStone13 <thetstone13@gmail.com>
2016-10-08 23:36:09 -0600

    3 bytes per pixel
    2/3 reduction in number of pixels changed.
    Also travis are smart.

--- Steganography/src/MAIN/Matroschka.java
+++ Steganography/src/MAIN/Matroschka.java
@@ -81,6 +81,192 @@ public class Matroschka {
 	}
 	
 	
+	
+	public void hideMessage(BufferedImage image, byte[] encryptedMessage)
+	{
+		int x = 0;
+		int y = 0;
+		for(int b = 0; b < encryptedMessage.length; b = b + 3)
+		{
+			
+			int firstColor;
+			int secondColor;
+			int thirdColor;
+			
+			if(encryptedMessage[b] < 0)
+			{
+				firstColor = encryptedMessage[b] + 256;
+			}
+			else if(encryptedMessage[b] > 0)
+			{
+				firstColor = encryptedMessage[b];
+			}
+			else
+			{
+				firstColor = 0;
+			}
+			
+			
+			if(b+1 < encryptedMessage.length)
+			{
+				if(encryptedMessage[b+1] < 0)
+				{
+					secondColor = encryptedMessage[b+1] + 256;
+				}
+				else if(encryptedMessage[b+1] > 0)
+				{
+					secondColor = encryptedMessage[b+1];
+				}
+				else
+				{
+					secondColor = 0;
+				}
+			}
+			else
+			{
+				secondColor = 0;
+			}
+			
+			if(b+2 < encryptedMessage.length)
+			{
+				if(encryptedMessage[b+2] < 0)
+				{
+					thirdColor = encryptedMessage[b+2] + 256;
+				}
+				else if(encryptedMessage[b+2] > 0)
+				{
+					thirdColor = encryptedMessage[b+2];
+				}
+				else
+				{
+					thirdColor = 0;
+				}
+			}
+			else
+			{
+				thirdColor = 0;
+			}
+			
+			Color rgb = new Color(firstColor, secondColor, thirdColor);
+			System.out.println("Red = " + firstColor + " Green = " + secondColor + " Blue = " + thirdColor);
+			image.setRGB(x, y, rgb.getRGB());
+	
+			x++;
+			
+			if(x >= image.getWidth())
+			{
+				y++;
+				x=0;
+			}
+		}
+		
+		x = image.getWidth()-1;
+		y = image.getHeight()-1;
+		
+			String s = Integer.toString(encryptedMessage.length);
+			byte[] sizeAsBytes = s.getBytes();
+			for(byte b: sizeAsBytes)
+			{
+				if(b < 0)
+				{
+					//System.out.println("set image neg " + encryptedMessage[b]);
+					Color rgb = new Color(b + 256, 0, 0);
+					image.setRGB(x, y, rgb.getRGB());
+				}
+				else
+				{
+					//System.out.println("set image pos " + encryptedMessage[b]);
+					Color rgb = new Color(b, 0, 0);
+					image.setRGB(x, y, rgb.getRGB());
+				}
+				
+				x--;
+			}
+		
+			Color rgb = new Color(0, 1, 1);
+			image.setRGB(x, y, rgb.getRGB());
+			
+	}
+	
+	
+	
+	public byte[] getMessageFromImage(BufferedImage image)
+	{
+		int x = image.getWidth()-1;
+		int y = image.getHeight()-1;
+		int byteSize = 0;
+		Color c = new Color(image.getRGB(x, y));
+		
+		while(c.getBlue()== 0 && c.getGreen() == 0)
+		{
+			byteSize++;
+			x--;
+			c = new Color(image.getRGB(x, y));
+		}
+		x = image.getWidth()-1;
+		byte[] size = new byte[byteSize];
+		for(int u = 0; u < size.length; u++)
+		{
+			c = new Color(image.getRGB(x - u, y));
+			int val = c.getRed();
+			if(val > 127)
+			{
+				val = val-256;
+			}
+			size[u] = Byte.parseByte(Integer.toString(val));
+		}
+		int numBytes = Integer.parseInt(new String(size));
+
+		x = 0;
+		y = 0;
+		byte[] encryptedMessage = new byte[numBytes];
+		for(int i = 0; i < encryptedMessage.length; i++)
+		{
+			c = new Color(image.getRGB(x, y));
+			int byteVal;
+			switch(i % 3)
+			{
+				case 0:
+					byteVal = c.getRed();
+					if(byteVal > 127)
+					{
+						byteVal = byteVal-256;
+					}
+					encryptedMessage[i] = Byte.parseByte(Integer.toString(byteVal));
+					break;
+				case 1: 
+					byteVal = c.getGreen();
+					if(byteVal > 127)
+					{
+						byteVal = byteVal-256;
+					}
+					encryptedMessage[i] = Byte.parseByte(Integer.toString(byteVal));
+					break;
+				case 2: 
+					byteVal = c.getBlue();
+					if(byteVal > 127)
+					{
+						byteVal = byteVal-256;
+					}
+					encryptedMessage[i] = Byte.parseByte(Integer.toString(byteVal));
+					x++;
+					break;
+			}
+			
+			if(x >= image.getWidth())
+			{
+				x = 0;
+				y ++;
+			}
+		}
+		
+		return encryptedMessage;
+	}
+	
+	
+	
+	
+	/*
 	public void hideMessage(BufferedImage image, byte[] encryptedMessage)
 	{
 		int x = 0;
@@ -194,5 +380,5 @@ public class Matroschka {
 		return encryptedMessage;
 	}
 	
-	
+	*/
 }

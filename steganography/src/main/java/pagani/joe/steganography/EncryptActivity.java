02877a884374514d445707d02a841eed43472741
deadmau6 <joepop16@gmail.com>
2016-10-08 21:57:40 -0600

    still opening an image

--- steganography/src/main/java/pagani/joe/steganography/EncryptActivity.java
+++ steganography/src/main/java/pagani/joe/steganography/EncryptActivity.java
@@ -3,12 +3,13 @@ package pagani.joe.steganography;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
+import android.util.Log;
 /**
  * Created by User on 10/8/2016.
  */
@@ -16,7 +17,7 @@ import android.widget.Toast;
 public class EncryptActivity extends Activity implements View.OnClickListener {
     public static Context mContext;
     private Button selectImg;
-    private Intent openImg;
+    private String selectedImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +31,42 @@ public class EncryptActivity extends Activity implements View.OnClickListener {
     public void onClick(View v){
         switch (v.getId()){
             case R.id.selectImgBtn:
-                //go encrypt
                 selectImage();
                 break;
         }
     }
     public void selectImage(){
         Intent openImg = new Intent();
-        openImg.setAction(Intent.ACTION_VIEW);
-        openImg.setDataAndType(Uri.parse("content://media/external/images/ 1"), "image/*");
-        startActivity(openImg);
+        openImg.setType("image/*");
+        openImg.setAction(Intent.ACTION_OPEN_DOCUMENT);
+        startActivityForResult(Intent.createChooser(openImg, "Select Image"), 1);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                Uri imgUri = data.getData();
+                selectedImg = getSelectedImgPath(imgUri);
+                Log.d("Path: ", selectedImg + "blank");
+            }
+        }
+    }
+
+    public String getSelectedImgPath(Uri uri){
+        if (uri == null) {
+            return null;
+        }
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        cursor.moveToFirst();
+        if (cursor != null) {
+            int index = cursor.getColumnIndexOrThrow(projection[0]);
+            String path = cursor.getString(index);
+            cursor.close();
+            return path;
+        }
+        cursor.close();
+        return uri.getPath();
     }
 }

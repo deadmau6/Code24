610a89506073f632193e334595c52373334657c6
deadmau6 <joepop16@gmail.com>
2016-10-08 23:33:04 -0600

    Total Update

--- steganography/src/main/java/pagani/joe/steganography/DecryptActivity.java
+++ steganography/src/main/java/pagani/joe/steganography/DecryptActivity.java
@@ -3,8 +3,11 @@ package pagani.joe.steganography;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,10 +17,11 @@ import android.widget.Toast;
  */
 
 public class DecryptActivity extends Activity implements View.OnClickListener {
+    private static final int READ_REQUEST_CODE = 42;
     public static Context mContext;
     private Button selectImg;
     private Button decryptBtn;
-    //private Intent openImg;
+    private String selectedImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +45,38 @@ public class DecryptActivity extends Activity implements View.OnClickListener {
                 break;
         }
     }
-
-    public void selectImage() {
-        Intent openImg = new Intent();
-        openImg.setAction(Intent.ACTION_VIEW);
-        openImg.setDataAndType(Uri.parse("content://media/external/images/ 1"), "image/*");
-        startActivity(openImg);
+    public void selectImage(){
+        Intent openImg = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        openImg.addCategory(Intent.CATEGORY_OPENABLE);
+        openImg.setType("image/*");
+        startActivityForResult(openImg, READ_REQUEST_CODE);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (resultCode == Activity.RESULT_OK && requestCode == READ_REQUEST_CODE){
+            Uri imgUri = null;
+            if (data != null) {
+                imgUri = data.getData();
+                Log.d("Path: ", imgUri.toString());
+                getSelectedImgPath(imgUri);
+            }
+        }
+    }
+
+    public void getSelectedImgPath(Uri uri){
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        try {
+            if (cursor != null && cursor.moveToFirst()) {
+                selectedImg = cursor.getString(
+                        cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                );
+                Log.d("Display Name ", selectedImg);
+            }
+        } finally {
+            cursor.close();
+        }
+    }
     public void decryptActivity() {
         Intent startMessageActivity = new Intent(this, MessageActivity.class);
         startActivity(startMessageActivity);
